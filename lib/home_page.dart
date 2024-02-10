7f273efa7eaa758c0c41ac3324b16b536bd7faf6
Nirsu <meru.fontaine@gmail.com>
2024-02-11 00:53:16 +0100

    [FEAT]: add logic when accept

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,8 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:valentine/themes/theme_extension.dart';
 import 'package:valentine/widgets/custom_button.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _hasAccepted = false;
+
+  void _onAccept() {
+    setState(() {
+      _hasAccepted = true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,30 +26,32 @@ class HomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Image.asset(
-              'assets/gifs/cute_love_bear_roses.gif',
+              'assets/gifs/${_hasAccepted ? 'bear_kisses' : 'cute_love_bear_roses'}.gif',
               height: 200,
               width: 200,
             ),
             Text(
-              'Will you be my Valentine?',
+              _hasAccepted ? 'Yay!!! 😀❤️' : 'Will you be my Valentine?',
               style: context.theme.textTheme.titleLarge,
             ),
-            const SizedBox(height: 8),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                CustomButton(
-                  onPressed: () {},
-                  label: 'Yes',
-                ),
-                const SizedBox(width: 8),
-                CustomButton(
-                  onPressed: () {},
-                  label: 'No',
-                  backgroundColor: Colors.red,
-                ),
-              ],
-            )
+            if (!_hasAccepted) ...<Widget>[
+              const SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CustomButton(
+                    onPressed: _onAccept,
+                    label: 'Yes',
+                  ),
+                  const SizedBox(width: 8),
+                  CustomButton(
+                    onPressed: () {},
+                    label: 'No',
+                    backgroundColor: Colors.red,
+                  ),
+                ],
+              ),
+            ],
           ],
         ),
       ),
